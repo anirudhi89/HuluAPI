e02e34b385cb50860c44071e49ac8ece0a6a0a15
Anirudh Iyer <anirudh.iyer@outlook.com>
2022-07-16 19:02:33 -0400

    login and go to homescreen done

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # HuluAPI
 Hulu API, using Puppeteer, NodeJS and Headless Chrome
+
+## Capabilities
+- Login to Hulu
+- 
+
+## ToDo:
+- Refactor into API
+- Allow specific queries through request parameters
+
+
+
+
+###### Last Updated: 7/16/22
--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const puppeteer = require('puppeteer');
+require('dotenv').config()
+var pwd = process.env.PWD; //temp
+var uname = process.env.UNAME; //temp
 
-await function getMovies() {
+async function getMovies() {
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null
     })
 
     const page = await browser.newPage();
-    const url = `https://hulu.com`
+    const url = `https://hulu.com/welcome`
+    await page.goto(url);
+    
+    //login sequence
+    await page.click('.navigation__login-button.navigation__logged-out-button');
+    await page.type('#email_id', uname)
+    await page.type('#password_id', pwd)
+    await page.click('jsx-4282396490.login-button')
+
+    // todo: param headers to specify movies, tv shows, genre (movies & TV shows), kid friendly, ratings, etc
+    var listoftv = [];
+    var listofmovies = [];
+
 
 }
+
+getMovies()
