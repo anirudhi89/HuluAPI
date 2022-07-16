2094b650c58f50183bef2599e4875d63e5db1531
Anirudh Iyer <anirudh.iyer@outlook.com>
2022-07-16 19:49:11 -0400

    nav to movies

--- index.js
+++ index.js
@@ -10,19 +10,31 @@ async function getMovies() {
     })
 
     const page = await browser.newPage();
-    const url = `https://hulu.com/welcome`
+    var url = `https://hulu.com/welcome`
     await page.goto(url);
     
     //login sequence
     await page.click('.navigation__login-button.navigation__logged-out-button');
     await page.type('#email_id', uname)
     await page.type('#password_id', pwd)
-    await page.click('jsx-4282396490.login-button')
+    await page.click('.login-button')
+    await page.waitForNavigation();
 
     // todo: param headers to specify movies, tv shows, genre (movies & TV shows), kid friendly, ratings, etc
+    // Navigate to TV Shows or movies, depending on what is selected (for now only tv shows)
+    const tvshow = true; //temp
+    var tvshowurl = "https://hulu.com/hub/tv";
+    var otherurl = "https://hulu.com/hub/movies";
+    var selectedstr = tvshow ? tvshowurl : otherurl;
+    console.log(selectedstr)
+    await page.goto(selectedstr)
+
+
+
     var listoftv = [];
     var listofmovies = [];
 
+    
 
 }
 
